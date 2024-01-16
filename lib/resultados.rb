c5a32d5cde25e318044fe7398d8d5edb4d85b787
vinicius ferreira <viniciustferreira@gmail.com>
2024-01-16 18:39:12 -0300

    refactoring the class: remove useless code and add condition to avoid division by zero

--- lib/resultados.rb
+++ lib/resultados.rb
@@ -4,16 +4,14 @@ class Resultados
   def initialize()
     @valor_meta = valor_meta
     @valor_realizado = valor_realizado
-    # @valor_performance = valor_performance
   end
 
   def calcula_performance
-    return 0 if valor_meta.zero?
+    return 0 if valor_meta.nil? || valor_meta.zero?
     valor_realizado / valor_meta
   end
 
   def calcula_realizado
-    return 0 if valor_realizado.zero?
     valor_meta * valor_peformance
   end
 end
