af6f09043baae599e3a1e3d8dfa03b826ae2a182
vinicius ferreira <viniciustferreira@gmail.com>
2024-01-16 21:27:01 -0300

    fix corner cases with types insert params on constructor

--- lib/resultados.rb
+++ lib/resultados.rb
@@ -1,17 +1,19 @@
 class Resultados
   attr_accessor :valor_meta, :valor_realizado
 
-  def initialize()
+  def initialize(valor_meta, valor_realizado)
     @valor_meta = valor_meta
     @valor_realizado = valor_realizado
   end
 
   def calcula_performance
+    return 0 if valor_realizado.nil?
     return 0 if valor_meta.nil? || valor_meta.zero?
     valor_realizado / valor_meta
   end
 
   def calcula_realizado
-    valor_meta * valor_peformance
+    return 0 if valor_meta.nil? || valor_realizado.nil?
+    valor_meta * valor_realizado
   end
 end
